a6d019d0c924ea8d35d3ed655f188e5efb69986e
Dgliump <lortkiphanidzeliaa@gmail.com>
2023-07-24 02:50:13 -0400

    renew

--- exForMeths.js
+++ exForMeths.js
@@ -1,7 +1,9 @@
 // filter() ---> array.filter(function(currentValue,index,arr),thisValue);
 const apartNums = [123,5,11,10,89,32,54];
 const result = apartNums.filter((apart)=>{
-    return apart <= 100;
+    if(apart !== 5 && apart <= 100){  // if ther is false, filter removes from result.
+    return apart;
+    }
 });
 console.log("filter:", result.sort((a,b)=>{return a-b}));
 
@@ -9,6 +11,10 @@ console.log("filter:", result.sort((a,b)=>{return a-b}));
 const picture = ['picture','picture','picture','picture'];
 picture.forEach((item,index,arr)=>{
     arr[index] = item + '-' + (index+1); 
+    if(index !== 1){
+        return arr[index];
+    } 
+    // just for check diff between filter & forEach 
 })
 console.log("forEach:", picture);
 
@@ -21,12 +27,27 @@ const streetRes = streetNum.map((element)=>{
     return "165/Wolfstreet/ " + element +",  Zip code: 11324";
     }
 });
+console.log(streetNum);
 console.log(streetRes);
+
+
+// reduce() ---> array.reduce((sum,elem) => sum +elem);
+
+let redd = [1,2,3,4,5,6,7,8,9];
+let sum = redd.reduce((sum,elem) => sum + elem);
+console.log(sum);
 
 // concat() ----> array1.concat(array2,array3);
 
 // sort() ----> array.sort(compareFunction);
 // spred ooperator ---> array1.concat(...array2, ...array3);
 
+
+
+//Hoisting
+
+let ho = 5;
+console.log(ho * hoi); // ReferenceError
+let hoi = 0; 
 
 
