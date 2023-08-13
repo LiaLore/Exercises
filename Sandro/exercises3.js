71d8b3a73f8a78fa0fb9417da315e8bba6cc6f69
Dgliump <lortkiphanidzeliaa@gmail.com>
2023-08-13 02:41:09 -0400

    renew 08 / 13

--- Sandro/exercises3.js
+++ Sandro/exercises3.js
@@ -117,29 +117,35 @@ btn.addEventListener("click", ()=>{
 //     name, and returns an array of unique values for that property 
 //     across all objects.
 
-let array49 = [{
+let array48 = [{
     firstName: "Lika",
     lastName: "Lore",
 },{
     country: "USA",
     city: "Wilton",
 }];
-console.log(array49);
-console.log(array49[1]);
-console.log(array49[1].city);
+console.log("48: " +array48);
+console.log("48: " +array48[1]);
+console.log("48: " +array48[1].city);
 
 
 // It's not a function!!
-//  To be continued...
 
 // 49. Write a function that takes a string as input and returns an object 
 //     containing the count of each character in the string, excluding spaces.
 
+let string49 = "I am Lia and kal b b bdsds dadmv,bdfertrtrhgsfdfv, lopqe sdafsgdjfl mznxbvcz";
+let countABC = "b";
 
-
-
-
-
+function countChar (string,count){
+    let result = 0;
+    for (let i = 0; i < string.length; i++){
+        if (string.charAt(i) == count) 
+        result++
+    }
+    return result;
+}
+console.log(countChar(string49,countABC));
 
 // 50. Write a function that takes a string as input and returns the 
 //     longest substring that contains only unique characters. 
@@ -148,42 +154,155 @@ console.log(array49[1].city);
 
 
 
+function findUniqueChar(){
+    let string50 = "return the first one you encounter."
+    // string50.slice(" ").toString();
+    string50 = string50.split("the");
+    // string50 = new Set(string50); // I need to learn more about it!!
+    string50 =[...string50].join("");
+    console.log(string50);
+}
+console.log(findUniqueChar());
 
 
-// 51. For Loop: Write a program that prints the even numbers between 
-//      1 and 20 using a for loop. 
+// 51. For Loop: 
+//     Write a program that prints the even numbers between 1 and 20 
+//     using a for loop. 
+
+let nums51 = [];
+for (let i = 1; i <= 20; i++){
+    if(i % 2 == 0){
+        nums51.push(i)
+    }
+    // return nums51;
+}
+console.log(nums51)
+
+// 52. Switch/Case:  
+//     Write a program that takes a number as input and prints the 
+//     corresponding day of the week using a switch/case statement. 
+//     For example, if the input is 1, it should print "Sunday."
+
+let day;
+switch(new Date().getDay()){
+    case 0:
+        day = "Sunday";
+        break;
+    case 1:
+        day = "Monday";
+        break;   
+    case 2:
+        day = "Tuesday";
+        break;    
+    case 3:
+        day = "Wednesday";
+        break;    
+    case 4:
+        day = "Thursday";
+        break;    
+    case 5:
+        day = "Friday";
+        break;    
+    case 6:
+        day = "Saturday";
+        break;
+}
+console.log("Today is " + day);
+
+//NOTE: I need to learn more about it!! 
+
+// 53. While Loop: 
+//     Write a program that asks the user to enter a number and keeps 
+//     printing "Hello!" that many times using a while loop.
+
+let btn53 = document.getElementById("btn53");
+btn53.addEventListener("click", ()=>{
+    let input = prompt("Enter Number:");
+    let hello = 0;
+    while (hello < input){
+        hello++;
+        console.log("Hello!");
+    }
+})
 
 
+// 54. For Loop: 
+//     Write a program that calculates the sum of numbers from 1 to 100 
+//     using a for loop and prints the result.
 
-// 52. Switch/Case:  Write a program that takes a number as input and 
-//     prints the corresponding day of the week using a switch/case 
-//     statement. For example, if the input is 1, it should print 
-//     "Sunday."
+let sum54 = 0;
+for(let i = 0; i <= 100; i++){
+    sum54 = sum54+i;
+}
+console.log(sum54);
 
-
-
-
-// 53. While Loop: Write a program that asks the user to enter a number 
-//     and keeps printing "Hello!" that many times using a while loop.
-
-
-
-
-// 54. For Loop: Write a program that calculates the sum of numbers from 
-//     1 to 100 using a for loop and prints the result.
-
-
-
-// 55. Switch/Case: Write a program that takes a month number as input 
+// 55. Switch/Case: 
+//     Write a program that takes a month number as input 
 //     (1 for January, 2 for February, etc.) and prints the number of 
 //      days in that month using a switch/case statement.
 
+let btn55 = document.getElementById("btn55");
+btn55.addEventListener("click", () => {
+
+    let monthNumber = prompt("Enter the month number:");
+    let month = "";
+
+    switch(monthNumber){
+        case 1: month = "January";
+        break;        
+        case 2: month = "February";
+        break;        
+        case 3: month = "March";
+        break;        
+        case 4: month = "April";
+        break;        
+        case 5: month = "May";
+        break;        
+        case 6: month = "June";
+        break;        
+        case 7: month = "July";
+        break;        
+        case 8: month = "August";
+        break;        
+        case 9: month = "September";
+        break;        
+        case 10: month = "October";
+        break;        
+        case 11: month = "November";
+        break;       
+        case 12: month = "December";
+        break;
+    }
+    if(monthNumber == 1 || monthNumber == 3 || monthNumber == 5 || monthNumber == 7 ||
+        monthNumber == 8 || monthNumber == 10 || monthNumber == 12){
+            console.log("In " + monthNumber + " are 31 days.");
+        }else if(monthNumber == 4 || monthNumber == 6 || monthNumber == 9 || monthNumber == 7 ||
+            monthNumber == 11){
+                console.log("In" + monthNumber + " are 30 days.")
+        }else if (monthNumber == 2){
+            console.log("In " + monthNumber + " are 28 days, but every 4th years are 29 days." );
+
+        }else{
+            console.log("Wrong Input!");
+        }
+});
+
+// Note: how can I print month name in log????????
+
+
+// to be continud......
 
 
 
-// 56. While Loop: Write a program that asks the user to guess a secret 
-//     number between 1 and 10. Keep asking for guesses until they guess 
-//      correctly using a while loop.
+
+
+
+
+
+// 56. While Loop: 
+//     Write a program that asks the user to guess a secret number
+//     between 1 and 10. Keep asking for guesses until they guess 
+//     correctly using a while loop.
 
 
 
--- WrongCode.js
+++ WrongCode.js
@@ -49,3 +49,99 @@ for (let i = 0; i <= nums.length; i++){
 }
 console.log(nums)
 console.log(largest11);
+
+
+
+//49 
+let string49 = "I am Lia";
+let countA = 0;
+let countB = 0;
+let countC = 0;
+let countD = 0;
+let countE = 0;
+let countF = 0;
+let countG = 0;
+let countH = 0;
+let countI = 0;
+let countJ = 0;
+let countK = 0;
+let countL = 0;
+let countM = 0;
+let countN = 0;
+let countO = 0;
+let countP = 0;
+let countQ = 0;
+let countR = 0;
+let countS = 0;
+let countT = 0;
+let countU = 0;
+let countV = 0;
+let countW = 0;
+let countX = 0;
+let countY = 0;
+let countZ = 0;
+function charCounter(str){
+for (let i =  0; i > str.length; i++){
+    if(str[i] === "a" || str[i] === "A"){
+        countA ++;
+    }else if(str[i] === "b" || str[i] === "B"){
+        countB++
+    }else if(str[i] === "c" || str[i] === "C"){
+        countC++
+    }else if(str[i] === "d" || str[i] === "D"){
+        countD++
+    }else if(str[i] === "e" || str[i] === "E"){
+        countE++
+    }else if(str[i] === "f" || str[i] === "F"){
+        countF++
+    }else if(str[i] === "g" || str[i] === "G"){
+        countG++
+    }else if(str[i] === "h" || str[i] === "H"){
+        countH++
+    }else if(str[i] === "i" || str[i] === "I"){
+        countI++
+    }else if(str[i] === "j" || str[i] === "J"){
+        countJ++
+    }else if(str[i] === "k" || str[i] === "K"){
+        countK++
+    }else if(str[i] === "l" || str[i] === "L"){
+        countL++
+    }else if(str[i] === "m" || str[i] === "M"){
+        countM++
+    }else if(str[i] === "n" || str[i] === "N"){
+        countN++
+    }else if(str[i] === "o" || str[i] === "O"){
+        countO++
+    }else if(str[i] === "p" || str[i] === "P"){
+        countP++
+    }else if(str[i] === "q" || str[i] === "Q"){
+        countQ++
+    }else if(str[i] === "r" || str[i] === "R"){
+        countR++
+    }else if(str[i] === "s" || str[i] === "S"){
+        countS++
+    }else if(str[i] === "t" || str[i] === "T"){
+        countT++
+    }else if(str[i] === "u" || str[i] === "U"){
+        countU++
+    }else if(str[i] === "v" || str[i] === "V"){
+        countV++
+    }else if(str[i] === "w" || str[i] === "W"){
+        countW++
+    }else if(str[i] === "x" || str[i] === "X"){
+        countX++
+    }else if(str[i] === "y" || str[i] === "Y"){
+        countY++
+    }else if(str[i] === "z" || str[i] === "Z"){
+        countZ++
+    }
+}
+console.log(` A: ${countA}  B: ${countB}  C: ${countC} D: ${countD} 
+E: ${countE} F: ${countF}  G: ${countG} H: ${countH} I: ${countI} 
+J: ${countJ} K: ${countK} L: ${countL} M: ${countM} N: ${countN} 
+O: ${countO} P: ${countP} Q: ${countQ} R: ${countR} S: ${countS} 
+T: ${countT} U: ${countU} V: ${countV} W: ${countW} X: ${countX} 
+Y: ${countY} Z: ${countZ} `)
+}
+console.log("49: " + charCounter(string49));
+
