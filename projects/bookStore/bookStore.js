b13a96289adc7c6915a6823eb7054997ab5fedac
Dgliump <lortkiphanidzeliaa@gmail.com>
2023-08-17 02:30:00 -0400

    enew 08/17

---
+++ projects/bookStore/bookStore.js
@@ -0,0 +1,7 @@
+const menuSign = document.getElementById("menuSign");
+menuSign.addEventListener("click", ()=> { })
+const signIn = document.getElementById("signIn");
+signIn.addEventListener("click", ()=> {
+    signIn.style.display="visible"
+})
+// I need insted of click to touch menu sign
